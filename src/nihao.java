f0d27ef7dbe262baf8da107aaadd8ffbe877d11a
itcast <784894729@qq.com>
2019-04-12 19:31:05 +0800

    搜素

--- src/nihao.java
+++ src/nihao.java
@@ -1,5 +1,6 @@
 public class nihao {
     public static void main(String[] args) {
         System.out.println("你好");
+		System.out.println("你好");System.out.println("你好");System.out.println("你好");
     }
 }
